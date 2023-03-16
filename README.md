4fb905f4fd8de8c1c2252039631f048a1ccecc40
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-03-17 04:45:54 +0530

    initial files

---
+++ README.md
@@ -0,0 +1,3 @@
+# i_doc_app
+
+This is the i-doc app for the Google Solution Challenge 2023.
---
+++ android/app/src/main/kotlin/com/example/i_doc_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.i_doc_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
